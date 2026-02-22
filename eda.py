ab71ad98692b0b2ee7fb381164ed5a21e5eb84b7
Caroline <carolineliu511@gmail.com>
2026-02-21 20:36:22 -0800

    added other eda code

--- eda.py
+++ eda.py
@@ -4,6 +4,16 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("data/processed/violence_data_cleaned.csv")
 
 
+#View overall distribution
+df['Value'].hist(bins=20)
+plt.show()
+
+
+#Compare demographic categories
+edu_df = df[df['Demographics Question'] == 'Education']
+print(edu_df.groupby('Demographics Response')['Value'].mean().sort_values())
+
+
 #Compare age groups
 age_df = df[df['Demographics Question'] == 'Age']
 print(age_df.groupby('Demographics Response')['Value'].mean())
